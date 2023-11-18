9275ef5fd293c8595d6e05651f5058c208c2a615
owner <lucas.s.derosa@gmail.com>
2023-11-17 23:31:20 -0600

    added EOR, ORR

--- Team16_Project2.go
+++ Team16_Project2.go
@@ -393,8 +393,10 @@ func simulateInstruction(simOutput string, list []Instruction, registry []int, d
 				cycle++
 				//*****ORR INSTRUCTION*****
 			case opcode == 1360:
+				regDest := registry[list[i].rn] | registry[list[i].rm]
+				registry[list[i].rd] = regDest
 				fmt.Fprintf(simOutputFile, "============\n")
-				fmt.Fprintf(simOutputFile, "Cycle:%d\t%d\t%s\t\n", cycle, list[i].memLoc, list[i].op)
+				fmt.Fprintf(simOutputFile, "Cycle:%d\t%d\t%s R%d, R%d, R%d\n", cycle, list[i].memLoc, list[i].op, list[i].rd, list[i].rn, list[i].rm)
 				fmt.Fprintf(simOutputFile, "registers:\n")
 				fmt.Fprintf(simOutputFile, "r00:\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t\n", registry[0], registry[1], registry[2], registry[3], registry[4], registry[5], registry[6], registry[7])
 				fmt.Fprintf(simOutputFile, "r08:\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t\n", registry[8], registry[9], registry[10], registry[11], registry[12], registry[13], registry[14], registry[15])
@@ -546,8 +548,10 @@ func simulateInstruction(simOutput string, list []Instruction, registry []int, d
 				cycle++
 				//*****EOR*****
 			case opcode == 1872:
+				regDest := registry[list[i].rn] ^ registry[list[i].rm]
+				registry[list[i].rd] = regDest
 				fmt.Fprintf(simOutputFile, "============\n")
-				fmt.Fprintf(simOutputFile, "Cycle:%d\t%d\t%s\t\n", cycle, list[i].memLoc, list[i].op)
+				fmt.Fprintf(simOutputFile, "Cycle:%d\t%d\t%s R%d, R%d, R%d\n", cycle, list[i].memLoc, list[i].op, list[i].rd, list[i].rn, list[i].rm)
 				fmt.Fprintf(simOutputFile, "registers:\n")
 				fmt.Fprintf(simOutputFile, "r00:\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t\n", registry[0], registry[1], registry[2], registry[3], registry[4], registry[5], registry[6], registry[7])
 				fmt.Fprintf(simOutputFile, "r08:\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t\n", registry[8], registry[9], registry[10], registry[11], registry[12], registry[13], registry[14], registry[15])
